5c17e3d66355472b19437176d2bf2dadcf60c8b6
Onkar Jadhav <omjadhav2610@gmail.com>
2023-02-28 07:21:11 +0000

    uncomment test route. use correct stratergy

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,29 +1,41 @@
 import { Controller, Post, UseGuards, Get, Req } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { Request } from 'express';
-// import { Role } from 'src/types/role.enum';
+import { Role } from 'src/types/role.enum';
 import { AuthService } from './auth.service';
-// import { Roles } from './role.decorator';
-// import { RolesGuard } from './roles.guard';
+import { Roles } from './role.decorator';
+import { RolesGuard } from './roles.guard';
 
 @Controller('auth')
 export class AuthController {
 
     constructor(private authService : AuthService){}
 
-    // @UseGuards(AuthGuard("jwt"), RolesGuard)
-    // @Get("/status")
-    // @Roles(Role.Admin)
-    // test(@Req() req: Request){
-    //     return req.user;
-    // }
+    @UseGuards(AuthGuard("jwt"), RolesGuard)
+    @Get("/status")
+    @Roles(Role.Admin)
+    test(@Req() req: Request){
+        return req.user;
+    }
 
 
 
     // body must have {username, password}
     @UseGuards(AuthGuard('admin-local'))
     @Post('admin')
-    async login(@Req() req : Request) {
+    async loginAdmin(@Req() req : Request) {
+        return this.authService.login(req.user);
+    }
+
+    @UseGuards(AuthGuard('owner-local'))
+    @Post('owner')
+    async loginOwner(@Req() req : Request) {
+        return this.authService.login(req.user);
+    }
+
+    @UseGuards(AuthGuard('user-local'))
+    @Post('user')
+    async loginUser(@Req() req : Request) {
         return this.authService.login(req.user);
     }
 }
