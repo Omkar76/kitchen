defc4703f48c97ce08eed3be7670a10db675a1f6
Onkar Jadhav <omjadhav2610@gmail.com>
2023-02-28 07:21:43 +0000

    export strategy

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -5,9 +5,11 @@ import { AdminLocalStrategy } from './admin.local.strategy';
 import { AuthController } from './auth.controller';
 import { JwtModule } from '@nestjs/jwt';
 import { JwtStrategy } from './jwt.strategy';
+import { OwnerLocalStrategy } from './owner.local.strategy';
+import { UserLocalStrategy } from './user.local.strategy';
 
 @Module({
-  providers: [AuthService, AdminLocalStrategy, JwtStrategy],
+  providers: [AuthService, AdminLocalStrategy, OwnerLocalStrategy, UserLocalStrategy, JwtStrategy],
   imports: [PassportModule.register({session : true}), JwtModule.register({
     secret: "khdsf.gxmcvbkjzbgflhIna!@#l4k2l4hkwrnesrh1",
     signOptions: {
